7e7a3f6309bb3cb8e3404f36e1c06cef6922c620
timulibarri-SCE <tim.ulibarri@c-openai.com>
2026-06-26 00:25:11 -0800

    Connect Command Center sync endpoint

--- commandcenter/sync-config.js
+++ commandcenter/sync-config.js
@@ -1,5 +1,5 @@
 window.COMMAND_CENTER_SYNC_CONFIG = {
-  endpoint: "",
+  endpoint: "https://script.google.com/macros/s/AKfycbz14BNHwFlXw0FNxGkCH988UroD-L97z3dCQwQxUDb3oXOyLWwM_0-H71__4GhFu9hO/exec",
   namespace: "commandcenter",
   pollMs: 15000,
 };
